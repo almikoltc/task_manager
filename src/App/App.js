3a0442783f3eea21edf4ab6ea97b4f977419aeb1
almikoltc <almikoltc@yandex.ru>
2023-09-11 15:15:07 +0500

    start

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <p>
-                    test
+                    react
                 </p>
 
             </header>
